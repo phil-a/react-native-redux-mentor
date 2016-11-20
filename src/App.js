782c42b6558f5c5d35a6138fd2950a8c7d893614
Philip Alekseev <phil.alekseev@gmail.com>
2016-11-19 20:20:35 -0500

    Add firebase + config variables

--- src/App.js
+++ src/App.js
@@ -3,8 +3,23 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import firebase from 'firebase';
+import Config from 'react-native-config';
 
 class App extends Component {
+
+  componentWillMount() {
+    const firebase_config = {
+    apiKey: Config.FIREBASE_API_KEY,
+    authDomain: `${Config.FIREBASE_PROJECT_NAME}.firebaseapp.com`,
+    databaseURL: `https://${Config.FIREBASE_PROJECT_NAME}.firebaseio.com`,
+    storageBucket: `${Config.FIREBASE_PROJECT_NAME}.appspot.com`,
+    messagingSenderId: Config.FIREBASE_MESSAGE_ID
+    };
+
+    firebase.initializeApp(firebase_config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
