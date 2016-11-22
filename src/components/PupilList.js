705d8b0e87f63d6b2dd3b1f64eecb08f03d97b22
Philip Alekseev <phil.alekseev@gmail.com>
2016-11-21 20:51:13 -0500

    Format data for component

--- src/components/PupilList.js
+++ src/components/PupilList.js
@@ -1,15 +1,30 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { ListView, View, Text } from 'react-native';
 import { pupilsFetch } from '../actions';
 
 class PupilList extends Component {
 
   componentWillMount() {
     this.props.pupilsFetch();
+    this.createDataSource(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ pupils }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(pupils);
   }
 
   render() {
+    console.log(this.props);
     return (
       <View>
         <Text>Pupil</Text>
@@ -21,4 +36,12 @@ class PupilList extends Component {
   }
 }
 
-export default connect(null, { pupilsFetch })(PupilList);
+const mapStateToProps = state => {
+  const pupils = _.map(state.pupils, (val, uid) => {
+    return { ...val, uid };
+  });
+
+  return { pupils };
+}
+
+export default connect(mapStateToProps, { pupilsFetch })(PupilList);
