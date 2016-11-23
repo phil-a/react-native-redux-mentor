ce25f4f57e99617b403523fa58850c6d207d43be
Philip Alekseev <phil.alekseev@gmail.com>
2016-11-22 20:37:50 -0500

    Refactor form component out of create component

--- src/components/PupilCreate.js
+++ src/components/PupilCreate.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Picker, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { connect } from 'react-redux';
 import { pupilUpdate, pupilCreate } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Button } from './common';
+import PupilForm from './PupilForm';
+
 class PupilCreate extends Component {
 
   onButtonPress(){
@@ -14,41 +16,7 @@ class PupilCreate extends Component {
   render() {
     return (
       <Card>
-        <CardSection>
-          <Input
-            label="Name"
-            placeholder="John Smith"
-            value={this.props.name}
-            onChangeText={value => this.props.pupilUpdate({ prop: 'name', value })}
-          />
-        </CardSection>
-
-        <CardSection>
-          <Input
-            label="Phone"
-            placeholder="555-555-5555"
-            value={this.props.phone}
-            onChangeText={value => this.props.pupilUpdate({ prop: 'phone', value })}
-          />
-        </CardSection>
-
-        <CardSection>
-          <Text style={styles.pickerLabel}>Select Day</Text>
-          <Picker
-            selectedValue={this.props.shift}
-            onValueChange={value => this.props.pupilUpdate({ prop: 'shift', value })}
-            style={styles.picker}
-          >
-            <Picker.Item label="Monday" value="Monday" />
-            <Picker.Item label="Tuesday" value="Tuesday" />
-            <Picker.Item label="Wednesday" value="Wednesday" />
-            <Picker.Item label="Thursday" value="Thursday" />
-            <Picker.Item label="Friday" value="Friday" />
-            <Picker.Item label="Saturday" value="Saturday" />
-            <Picker.Item label="Sunday" value="Sunday" />
-          </Picker>
-        </CardSection>
-
+        <PupilForm {...this.props} />
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Create Pupil
@@ -58,18 +26,6 @@ class PupilCreate extends Component {
     );
   }
 }
-
-const styles = {
-  picker: {
-    width: 200,
-    marginLeft: 10
-  },
-  pickerLabel: {
-    fontSize: 18,
-    paddingLeft: 20,
-    paddingTop: 10
-  }
-};
 
 const mapStateToProps = (state) => {
   const { name, phone, shift } = state.pupilForm;
