13e2fad59f650361add11a6addd181719dc0ab6b
Philip Alekseev <phil.alekseev@gmail.com>
2016-11-22 20:30:33 -0500

    Pass object to creation form

--- src/components/PupilListItem.js
+++ src/components/PupilListItem.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 import { CardSection } from './common';
 
 class PupilListItem extends Component {
+
+  onRowPress() {
+    Actions.pupilCreate({ pupil: this.props.pupil });
+  }
+
   render() {
     const { name } = this.props.pupil;
-
     return (
-      <CardSection>
-        <Text style={styles.titleStyle}>
-          {name}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+        <View>
+          <CardSection>
+            <Text style={styles.titleStyle}>
+              {name}
+            </Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
