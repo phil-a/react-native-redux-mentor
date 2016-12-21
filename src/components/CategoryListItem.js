f6a1991eee303fc4822fa7ec8b952b98d2e4ca10
Philip Alekseev <phil.alekseev@gmail.com>
2016-12-21 00:20:55 -0500

    Add some styles

--- src/components/CategoryListItem.js
+++ src/components/CategoryListItem.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Text, TouchableWithoutFeedback, TouchableOpacity, View} from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { goalsFetch } from '../actions';
-import { Card, CardSection } from './common';
+import { Card, CardSection, Button } from './common';
 
 class CategoryListItem extends Component {
 
@@ -20,41 +20,44 @@ class CategoryListItem extends Component {
     Actions.goalEdit({ goal: goal });
   }
 
-  renderGoal(goal) {
+  renderGoal(goal, index) {
     return (
-      <TouchableWithoutFeedback onPress={() => this.onGoalPress(goal)}>
-        <View>
-          <CardSection>
-            <Text>
-              {goal.name}
-            </Text>
-          </CardSection>
+      <View key={goal.uid} style={styles.goalRowStyle}>
+        <View style={styles.goalTextWrapper}>
+          <Text style={styles.goalTextStyle}>
+            {goal.name}
+          </Text>
+        </View>
+        <View style={styles.editButtonStyle}>
+          <Button onPress={() => this.onGoalPress(goal)}>Edit</Button>
+        </View>
+      </View>
+    );
+  }
+
+  renderGoalsForCategory() {
+    return this.props.goals.map((goal, index, array) => (
+      (goal.category===this.props.category.name) ? this.renderGoal(goal, index) : null
+    ));
+  }
+
+  renderCategory() {
+    const { name } = this.props.category;
+    return (
+      <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+        <View style={styles.categoryRowStyle}>
+          <Text style={styles.titleStyle}>
+            { name }
+          </Text>
         </View>
       </TouchableWithoutFeedback>
     );
   }
 
-  renderGoalsForCategory() {
-    //debugger;
-    const goalItems = this.props.goals.map((goal) => (
-      (goal.category===this.props.category.name) ? this.renderGoal(goal) : null
-    ));
-    return goalItems;
-  }
-
   render() {
-    const { name } = this.props.category;
     return (
       <Card>
-        <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
-          <View>
-            <CardSection>
-              <Text style={styles.titleStyle}>
-                { name }
-              </Text>
-            </CardSection>
-          </View>
-        </TouchableWithoutFeedback>
+        {this.renderCategory()}
         {this.renderGoalsForCategory()}
       </Card>
     );
@@ -64,8 +67,36 @@ class CategoryListItem extends Component {
 const styles = {
   titleStyle: {
     fontSize: 18,
+    fontWeight: '600',
+    color: 'white',
     paddingLeft: 15
-  }
+  },
+  goalRowStyle:{
+    padding: 15,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  goalTextWrapper: {
+    flex: 6,
+  },
+  goalTextStyle: {
+    fontSize: 15,
+    paddingLeft: 15
+  },
+  editButtonStyle: {
+    flex: 1,
+  },
+  editTextStyle: {
+    flex: 1,
+    alignSelf: 'center'
+  },
+  categoryRowStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#007aff',
+    padding: 15,
+  },
 }
 
 const mapStateToProps = state => {
