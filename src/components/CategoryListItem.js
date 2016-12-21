44257f62efe6cc54d244d5d03910f491b2b361b1
Philip Alekseev <phil.alekseev@gmail.com>
2016-12-21 03:03:28 -0500

    Add Grid view

--- src/components/CategoryListItem.js
+++ src/components/CategoryListItem.js
@@ -1,12 +1,22 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Text, TouchableWithoutFeedback, TouchableOpacity, View} from 'react-native';
+import { Text, TouchableWithoutFeedback, TouchableOpacity, View, StyleSheet} from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { goalsFetch } from '../actions';
 import { Card, CardSection, Button } from './common';
+import Grid from 'react-native-grid-component';
 
 class CategoryListItem extends Component {
+
+  state: {data: Array<any>};
+
+  constructor(props: Object) {
+    super(props);
+    this.state = {
+      data: this.props.goals.filter((goal) => goal.category == this.props.category.name),
+    };
+  }
 
   componentWillMount() {
     this.props.goalsFetch();
@@ -18,6 +28,15 @@ class CategoryListItem extends Component {
 
   onGoalPress(goal) {
     Actions.goalEdit({ goal: goal });
+  }
+
+  _renderItem = (data: any, i: number) => {
+    return (
+      <View style={[{ backgroundColor: 'orange' }, styles.item]} key={i}>
+        <Text>{data.name}</Text>
+        <TouchableOpacity onPress={() => this.onGoalPress(data)}><Text>Edit</Text></TouchableOpacity>
+      </View>
+    );
   }
 
   renderGoal(goal, index) {
@@ -33,12 +52,6 @@ class CategoryListItem extends Component {
         </View>
       </View>
     );
-  }
-
-  renderGoalsForCategory() {
-    return this.props.goals.map((goal, index, array) => (
-      (goal.category===this.props.category.name) ? this.renderGoal(goal, index) : null
-    ));
   }
 
   renderCategory() {
@@ -58,7 +71,13 @@ class CategoryListItem extends Component {
     return (
       <Card>
         {this.renderCategory()}
-        {this.renderGoalsForCategory()}
+        <Grid
+          style={styles.list}
+          renderItem={this._renderItem}
+          data={this.state.data}
+          itemsPerRow={1}
+          itemHasChanged={(d1, d2) => d1 !== d2}
+        />
       </Card>
     );
   }
@@ -97,6 +116,25 @@ const styles = {
     backgroundColor: '#007aff',
     padding: 15,
   },
+  item: {
+    flex: 1,
+    height: 160,
+    margin: 1,
+  },
+  list: {
+    flex: 1,
+  },
+}
+
+const colors = [
+  '#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5',
+  '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50',
+  '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800',
+  '#FF5722', '#795548', '#9E9E9E', '#607D8B',
+];
+
+function generateRandomColorsArray(length) {
+  return Array.from(Array(length)).map(() => colors[Math.floor(Math.random() * colors.length)]);
 }
 
 const mapStateToProps = state => {
