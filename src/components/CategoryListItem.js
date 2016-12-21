75a4dd83a5ddad56f8d3d123968337d034f88c63
Philip Alekseev <phil.alekseev@gmail.com>
2016-12-21 03:27:07 -0500

    Gridify (#1)
    * Add some styles
    
    * Add Grid view
    
    * Use props instead of state
    
    * Remove unused grid package

--- src/components/CategoryListItem.js
+++ src/components/CategoryListItem.js
@@ -1,10 +1,11 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Text, TouchableWithoutFeedback, TouchableOpacity, View, StyleSheet} from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { goalsFetch } from '../actions';
-import { Card, CardSection } from './common';
+import { Card, CardSection, Button } from './common';
+import Grid from 'react-native-grid-component';
 
 class CategoryListItem extends Component {
 
@@ -20,42 +21,54 @@ class CategoryListItem extends Component {
     Actions.goalEdit({ goal: goal });
   }
 
-  renderGoal(goal) {
+  _renderItem = (data: any, i: number) => {
     return (
-      <TouchableWithoutFeedback onPress={() => this.onGoalPress(goal)}>
-        <View>
-          <CardSection>
-            <Text>
-              {goal.name}
-            </Text>
-          </CardSection>
+      <View style={[{ backgroundColor: 'orange' }, styles.item]} key={i}>
+        <Text>{data.name}</Text>
+        <TouchableOpacity onPress={() => this.onGoalPress(data)}><Text>Edit</Text></TouchableOpacity>
+      </View>
+    );
+  }
+
+  renderGoal(goal, index) {
+    return (
+      <View key={goal.uid} style={styles.goalRowStyle}>
+        <View style={styles.goalTextWrapper}>
+          <Text style={styles.goalTextStyle}>
+            {goal.name}
+          </Text>
+        </View>
+        <View style={styles.editButtonStyle}>
+          <Button onPress={() => this.onGoalPress(goal)}>Edit</Button>
+        </View>
+      </View>
+    );
+  }
+
+  renderCategory() {
+    const { name } = this.props.category;
+    return (
+      <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+        <View style={styles.categoryRowStyle}>
+          <Text style={styles.titleStyle}>
+            { name }
+          </Text>
         </View>
       </TouchableWithoutFeedback>
     );
   }
 
-  renderGoalsForCategory() {
-    //debugger;
-    const goalItems = this.props.goals.map((goal) => (
-      (goal.category===this.props.category.name) ? this.renderGoal(goal) : null
-    ));
-    return goalItems;
-  }
-
   render() {
-    const { name } = this.props.category;
     return (
       <Card>
-        <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
-          <View>
-            <CardSection>
-              <Text style={styles.titleStyle}>
-                { name }
-              </Text>
-            </CardSection>
-          </View>
-        </TouchableWithoutFeedback>
-        {this.renderGoalsForCategory()}
+        {this.renderCategory()}
+        <Grid
+          style={styles.list}
+          renderItem={this._renderItem}
+          data={this.props.goals.filter((goal) => goal.category == this.props.category.name)}
+          itemsPerRow={2}
+          itemHasChanged={(d1, d2) => d1 !== d2}
+        />
       </Card>
     );
   }
@@ -64,8 +77,44 @@ class CategoryListItem extends Component {
 const styles = {
   titleStyle: {
     fontSize: 18,
+    fontWeight: '600',
+    color: 'white',
     paddingLeft: 15
-  }
+  },
+  goalRowStyle:{
+    padding: 15,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  goalTextWrapper: {
+    flex: 6,
+  },
+  goalTextStyle: {
+    fontSize: 15,
+    paddingLeft: 15
+  },
+  editButtonStyle: {
+    flex: 1,
+  },
+  editTextStyle: {
+    flex: 1,
+    alignSelf: 'center'
+  },
+  categoryRowStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#007aff',
+    padding: 15,
+  },
+  item: {
+    flex: 1,
+    height: 160,
+    margin: 1,
+  },
+  list: {
+    flex: 1,
+  },
 }
 
 const mapStateToProps = state => {
