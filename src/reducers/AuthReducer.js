c56d01baca53b786e7c9aa1100fa1470543f340b
Philip Alekseev <phil.alekseev@gmail.com>
2016-12-25 23:30:42 -0500

    Persistent login (#2)
    * WIP - Persistent login
    
    - Add and implement Redux-Persist
    - Add actions + reducers for wreath
    - todo: update current user in goals
    
    * Complete login persistence
    
    * Clean up

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,3 +1,5 @@
+import {REHYDRATE} from 'redux-persist/constants'
+
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
@@ -23,9 +25,18 @@ export default (state = INITIAL_STATE, action) => {
     case LOGIN_USER:
       return { ...state, loading: true, error: '' };
     case LOGIN_USER_SUCCESS:
-      return { ...state, ...INITIAL_STATE, user: action.payload };
+      return { ...state, ...INITIAL_STATE, user: action.payload.user, email: action.payload.email, password: action.payload.password };
     case LOGIN_USER_FAIL:
       return { ...state, error: 'Authentication Failed.', password: '', loading: false };
+    case REHYDRATE:
+      var incoming = action.payload.auth
+      if (incoming) return {
+        ...state,
+        user: incoming.user,
+        email: incoming.email,
+        password: incoming.password,
+      };
+      return state;
     default:
       return state;
   };
