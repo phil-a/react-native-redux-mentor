73fe589e07baae445eef16589510f199d1d92f00
Philip Alekseev <phil.alekseev@gmail.com>
2016-12-27 13:12:16 -0500

    Color category (#4)
    * Add color picker to category
    
    * Update card styling
    
    * Update existing goal category when changing category name
    
    * Clean up

--- src/components/CategoryForm.js
+++ src/components/CategoryForm.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text, Picker } from 'react-native';
+import { TouchableOpacity, Text, Modal, View } from 'react-native';
 import { connect } from 'react-redux';
 import { categoryUpdate } from '../actions';
-import { CardSection, Input, Button } from './common';
+import { CardSection, Input, Button, Color } from './common';
+import { ColorPicker } from 'react-native-color-picker';
 
 class CategoryForm extends Component {
+
+  state = { modalVisible: false }
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible})
+  }
+
+  setColorInForm(value) {
+    this.props.categoryUpdate({ prop: 'color', value })
+    this.setModalVisible(false);
+  }
 
   render() {
     return (
@@ -17,17 +29,61 @@ class CategoryForm extends Component {
             onChangeText={value => this.props.categoryUpdate({ prop: 'name', value })}
           />
         </CardSection>
+        <CardSection>
+          <View style={styles.colorPickerContainer}>
+            <Modal
+                animationType={"slide"}
+                transparent={false}
+                visible={this.state.modalVisible}
+                onRequestClose={() => {alert("Modal has been closed.")}}
+                >
+               <View style={{flex: 1}}>
+                <ColorPicker
+                  oldColor={this.props.color}
+                  onColorSelected={value => this.setColorInForm(value)}
+                  onOldColorSelected={value => this.setColorInForm(value)}
+                  style={{flex: 1}}
+                />
+                  <TouchableOpacity onPress={() => {
+                    this.setModalVisible(!this.state.modalVisible)
+                  }}>
+                    <Text>Hide Modal</Text>
+                  </TouchableOpacity>
+                </View>
+              </Modal>
+
+              <View style={styles.colorPicker}>
+                <Text style={styles.pickerLabel}>Color</Text>
+                <TouchableOpacity
+                onPress={() => {this.setModalVisible(true)}}
+                style={[styles.colorSelect, {backgroundColor: this.props.color}]}>
+                </TouchableOpacity>
+              </View>
+
+            </View>
+        </CardSection>
       </View>
     );
   }
 }
 
 const styles = {
-  picker: {
-    width: 200,
-    marginLeft: 10
+  colorPickerContainer: {
+    flex: 1,
+  },
+  colorPicker: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  colorSelect: {
+    flex: 2,
+    height: 40,
+    width: 40,
+    borderWidth: 1,
+    borderRadius: 100,
   },
   pickerLabel: {
+    flex: 1,
     fontSize: 18,
     paddingLeft: 20,
     paddingTop: 10
@@ -35,9 +91,9 @@ const styles = {
 };
 
 const mapStateToProps = (state) => {
-  const {name} = state.categoryForm;
+  const {name, color} = state.categoryForm;
 
-  return { name }
+  return { name, color }
 };
 
 export default connect(mapStateToProps, { categoryUpdate })(CategoryForm);
